5fdd97fd62b84f4f40cbb8e27db847a23bc40d2d
SM Sakin <saklinemostaksakin58@example.com>
2024-05-17 11:43:15 +0600

    faq design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import HomePage from "./HomePage";
 import { useState } from "react";
 import Users from "../Users/Users";
 import Navbar from "../Navbar/Navbar";
+import Faq from "../Faq/Faq";
+import { FaQ } from "react-icons/fa6";
 
 const Home = () => {
   const [show, setShow] = useState("home");
@@ -13,11 +15,12 @@ const Home = () => {
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center justify-center">
-        <Navbar ></Navbar>
+        <Navbar></Navbar>
         {/* Page content here */}
-        <div className="w-full min-h-screen text-center mt-[20%]">
+        <div className="w-full min-h-screen text-center ">
           <HomePage show={show}></HomePage>
           <Users show={show}></Users>
+          <Faq show={show}></Faq>
         </div>
       </div>
       <div className="drawer-side">
@@ -26,7 +29,9 @@ const Home = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className={`menu p-4  min-h-full text-base-content w-60 bg-[#0f172a]`}>
+        <ul
+          className={`menu p-4  min-h-full text-base-content w-60 bg-[#0f172a]`}
+        >
           {/* Sidebar content here */}
           <Link
             onClick={() => {
@@ -64,6 +69,21 @@ const Home = () => {
               Users
             </Link>
           </li>
+
+          <li
+            onClick={() => {
+              setShow("faq");
+            }}
+            className="text-[15px] hover:bg-[#460073] rounded mt-2"
+          >
+            <Link>
+              <span>
+              <FaQ />
+              </span>
+              Faq
+            </Link>
+          </li>
+
         </ul>
       </div>
     </div>
